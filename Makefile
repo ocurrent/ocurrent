df088862f63e35e0836013a9ce815b53583d140c
Thomas Leonard <talex5@gmail.com>
2019-06-07 12:45:09 +0100

    Convert to expect tests
    The expected dot graphs are now stored in Git. If the output changes,
    use `dune promote` to accept the updates.
    
    Also, generate SVGs from the Makefile now to avoid creating a dune
    dependency on graphviz.

--- Makefile
+++ Makefile
@@ -1,7 +1,22 @@
 .PHONY: test build
 
+DOTS=$(sort $(wildcard _build/default/test/*.dot))
+
 test:
-	dune exec ./test/test.exe
+	if dune runtest; then make svgs; else make svgs; exit 1; fi
+
+svg:
+	mkdir svg
+
+svg/%.svg: svg _build/default/test/%.dot
+	@dot -Tsvg $^ -o $@
+
+# Create svg/* files from the test result dot files.
+svgs: $(addprefix svg/,$(addsuffix .svg,$(basename $(notdir ${DOTS}))))
 
 build:
 	dune build @all
+
+clean:
+	dune clean
+	rm -f svg/*.svg
--- test/test.ml
+++ test/test.ml
@@ -1,7 +1,5 @@
 open Current.Syntax    (* let*, let+, etc *)
 
-module StringMap = Map.Make(String)
-
 module Git = Current_git_test
 module Docker = Current_docker_test
 module Opam = Current_opam_test
@@ -65,7 +63,7 @@ let v5 commit =
 
 (* Test driver *)
 
-let output_dir = Fpath.v "./results"
+let output_dir = Fpath.v "."
 
 let or_die = function
   | Ok x -> x
@@ -80,16 +78,10 @@ let write_dot_to_channel ch x =
   Ok ()
 
 let write_dot ~name s =
-  let open Bos in
   let _ : bool = Bos.OS.Dir.create output_dir |> or_die in
   let dotfile = output_dir / Fmt.strf "%s.dot" name in
-  let svgfile = output_dir / Fmt.strf "%s.svg" name in
   Bos.OS.File.with_oc dotfile write_dot_to_channel s |> or_die |> or_die;
-  Fmt.pr "Wrote %a@." Fpath.pp dotfile;
-  let dot = Cmd.(v "dot" % "-Tsvg" % p dotfile % "-o" % p svgfile) in
-  match OS.Cmd.run dot with
-  | Ok () -> ()
-  | Error (`Msg x) -> failwith x
+  Fmt.pr "Wrote %a@." Fpath.pp dotfile
 
 let test_commit =
   Git.commit ~repo:"my/project" ~hash:"123"
@@ -105,12 +97,12 @@ let test ~name v =
   Fmt.pr "@.Before: %a@." Current.Analysis.pp s;
   Fmt.pr "--> %a@." (Current.Executor.pp_output (Fmt.unit "()")) x;
   Fmt.pr "Depends on: %a@." Fmt.(Dump.list string) inputs;
-  write_dot ~name:(name ^ "-before") s;
+  write_dot ~name:(name ^ ".1") s;
   Git.complete_clone test_commit;
   (* After supplying the input: *)
   let s, x, inputs = Current.Executor.run @@ v input in
   Fmt.pr "@.After: %a@." Current.Analysis.pp s;
-  write_dot ~name:(name ^ "-after") s;
+  write_dot ~name:(name ^ ".2") s;
   Fmt.pr "--> %a@." (Current.Executor.pp_output (Fmt.unit "()")) x;
   Fmt.pr "Depends on: %a@." Fmt.(Dump.list string) inputs
 
