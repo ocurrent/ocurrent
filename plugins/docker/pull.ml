d789f91a5dbe0406e9c10fcac73e466de2cb68ac
Thomas Leonard <talex5@gmail.com>
2019-07-01 10:51:24 +0100

    Return image digest from docker pull

--- plugins/docker/pull.ml
+++ plugins/docker/pull.ml
@@ -7,11 +7,15 @@ module Value = Image
 
 let id = "docker-pull"
 
-let build ~switch No_context job image =
-  let cmd = Array.of_list @@ ["docker"; "pull"; image] in
-  Current_cache.Process.exec ~switch ~job ("", cmd) >|= function
-  | Ok () -> Ok image
-  | Error _ as e -> e
+let build ~switch No_context job tag =
+  let cmd = Array.of_list @@ ["docker"; "pull"; tag] in
+  Current_cache.Process.exec ~switch ~job ("", cmd) >>= function
+  | Error _ as e -> Lwt.return e
+  | Ok () ->
+    let cmd = [| "docker"; "image"; "inspect"; tag; "-f"; "{{.Id}}" |] in
+    Lwt_process.pread_line ("", cmd) >|= fun id ->
+    Current_cache.Job.log job "Pulled %S -> %S" tag id;
+    Ok id
 
 let pp f x = Fmt.pf f "docker pull %s" x
 
