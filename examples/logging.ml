85a05fa9b5cc5fa6aea9757a5355fcbb120ff9f3
Thomas Leonard <talex5@gmail.com>
2019-08-22 09:41:53 +0100

    Report logging metrics via Prometheus

--- examples/logging.ml
+++ examples/logging.ml
@@ -1,9 +1,28 @@
 open Current.Syntax
 
+module Metrics = struct
+  open Prometheus
+
+  let namespace = "ocurrent"
+
+  let subsystem = "logs"
+
+  let inc_messages =
+    let help = "Total number of messages logged" in
+    let c =
+      Counter.v_labels ~label_names:[ "level"; "src" ] ~help ~namespace
+        ~subsystem "messages_total"
+    in
+    fun lvl src ->
+      let lvl = Logs.level_to_string (Some lvl) in
+      Counter.inc_one @@ Counter.labels c [ lvl; src ]
+end
+
 let reporter =
   let report src level ~over k msgf =
     let k _ = over (); k () in
     let src = Logs.Src.name src in
+    Metrics.inc_messages level src;
     msgf @@ fun ?header ?tags:_ fmt ->
     Fmt.kpf k Fmt.stdout ("%a %a @[" ^^ fmt ^^ "@]@.")
       Fmt.(styled `Magenta string) (Printf.sprintf "%14s" src)
