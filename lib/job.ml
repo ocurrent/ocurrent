5ca4bdda3467cc556ef7772dbc8ed6b9d3b3ccdb
Thomas Leonard <talex5@gmail.com>
2020-03-18 10:16:46 +0000

    Fix cancelling a job while waiting for confirmation
    We only stopped waiting when the job's switch was turned off, which
    now only happens after cancellation has succeeded.

--- lib/job.ml
+++ lib/job.ml
@@ -160,7 +160,7 @@ let with_handler t ~on_cancel fn =
 let confirm t ?pool level =
   let confirmed =
     let confirmed = Config.confirmed level t.config in
-    Switch.add_hook_or_fail t.switch (fun _ -> Lwt.cancel confirmed; Lwt.return_unit);
+    on_cancel t (fun _ -> Lwt.cancel confirmed; Lwt.return_unit) >>= fun () ->
     match Lwt.state confirmed with
     | Lwt.Return () -> Lwt.return_unit
     | _ ->
