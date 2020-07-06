59626a172fd404247eb293c973bfe2fbc742f98f
Thomas Leonard <talex5@gmail.com>
2020-07-06 13:44:18 +0100

    Docker: return the RepoId after pushing a manifest

--- plugins/docker/s.ml
+++ plugins/docker/s.ml
@@ -10,6 +10,7 @@ module type DOCKER = sig
     include Current_cache.S.WITH_DIGEST
     include Current_cache.S.WITH_MARSHAL with type t := t
 
+    val of_hash : string -> t
     val hash : t -> string
     val pp : t Fmt.t
   end
