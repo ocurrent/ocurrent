124eccf5d16354da213cb90ffb5860b65ba2f006
Patrick Ferris <patrick@sirref.org>
2022-06-21 09:46:39 +0100

    Allow users to provide a command printer

--- lib/process.mli
+++ lib/process.mli
@@ -1,11 +1,15 @@
+val pp_cmd : Format.formatter -> Lwt_process.command -> unit
+
 val exec :
   ?cwd:Fpath.t -> ?stdin:string ->
+  ?pp_cmd:(Format.formatter -> Lwt_process.command -> unit) ->
   ?pp_error_command:(Format.formatter -> unit) ->
   cancellable:bool -> job:Job.t -> Lwt_process.command ->
   unit Current_term.S.or_error Lwt.t
 
 val check_output :
   ?cwd:Fpath.t -> ?stdin:string ->
+  ?pp_cmd:(Format.formatter -> Lwt_process.command -> unit) ->
   ?pp_error_command:(Format.formatter -> unit) ->
   cancellable:bool -> job:Job.t -> Lwt_process.command ->
   string Current_term.S.or_error Lwt.t
