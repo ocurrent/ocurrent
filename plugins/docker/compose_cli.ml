1ddc6154abfed820715685f2ada55e3c1dd399c9
Mark Elvers <mark.elvers@tunbury.org>
2022-07-20 13:05:10 +0000

    Added compose_cli.ml

---
+++ plugins/docker/compose_cli.ml
@@ -0,0 +1,41 @@
+open Lwt.Infix
+
+type t = No_context
+
+let id = "docker-compose-cli"
+
+module Key = struct
+  type t = {
+    name : string;
+    docker_context : string option;
+  } [@@deriving to_yojson]
+
+  let digest t = Yojson.Safe.to_string (to_yojson t)
+end
+
+module Value = struct
+  type t = {
+    contents: string;
+  }
+
+  let digest { contents } =
+    Yojson.Safe.to_string @@ `Assoc [
+      "contents", `String contents
+    ]
+end
+
+module Outcome = Current.Unit
+
+let cmd args { Key.docker_context; name } =
+  Cmd.docker ~docker_context (["compose"; "-f"; "/dev/stdin"; "-p"; name ] @ args)
+
+let cmd_update = cmd ["up"]
+
+let publish No_context job key {Value.contents} =
+  Current.Job.start job ~level:Current.Level.Dangerous >>= fun () ->
+  Current.Process.exec ~stdin:contents ~cancellable:true ~job (cmd_update key)
+
+let pp f (key, { Value.contents }) =
+  Fmt.pf f "%a@.@[%a@]" Cmd.pp (cmd_update key) Fmt.string contents
+
+let auto_cancel = false
--- plugins/docker/s.ml
+++ plugins/docker/s.ml
@@ -88,6 +88,10 @@ module type DOCKER = sig
   val compose : name:string -> contents:string Current.t -> unit -> unit Current.t
   (** [service ~name ~image ~contents ()] keeps a Docker Compose deployment up-to-date.
       [contents] contains the full Compose Yaml file. *)
+
+  val compose_cli : name:string -> contents:string Current.t -> unit -> unit Current.t
+  (** [service ~name ~image ~contents ()] keeps a Docker Compose Cli deployment up-to-date.
+      [contents] contains the full Compose Yaml file. *)
 end
 
 module type HOST = sig
