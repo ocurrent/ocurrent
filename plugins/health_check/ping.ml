6c0111042bdca2e37767189a50c909a01b1fba3e
Mark Elvers <mark.elvers@tunbury.org>
2023-06-04 13:51:47 +0000

    Use ping6 for IPv6

--- plugins/health_check/ping.ml
+++ plugins/health_check/ping.ml
@@ -12,7 +12,10 @@ module Outcome = Current.Unit
 
 let publish No_context job key _ =
   Current.Job.start job ~level:Current.Level.Dangerous >>= fun () ->
-  Current.Process.exec ~cancellable:true ~job ("", [| "ping"; "-c"; "4"; key.Key.ip|])
+  let cmd = match String.contains key.Key.ip ':' with
+    | true -> "ping6"
+    | false -> "ping" in
+  Current.Process.exec ~cancellable:true ~job (cmd, [| cmd; "-c"; "4"; key.Key.ip|])
 
 let pp f (_, _) =
   Fmt.pf f "ping"
