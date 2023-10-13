d64a29fd6847bb280a509c85af042882a4ca5e96
benmandrew <benmandrew@gmail.com>
2023-10-13 16:44:23 +0200

    Correct full names

--- plugins/docker/metrics.ml
+++ plugins/docker/metrics.ml
@@ -5,20 +5,20 @@ let subsystem = "docker"
 
 let docker_pull_events =
   let help = "Incoming docker pull events" in
-  Counter.v ~help ~namespace ~subsystem "docker_pull_events"
+  Counter.v ~help ~namespace ~subsystem "pull_events"
 
 let docker_push_events =
   let help = "Outgoing docker push events" in
-  Counter.v ~help ~namespace ~subsystem "docker_push_events"
+  Counter.v ~help ~namespace ~subsystem "push_events"
 
 let docker_push_manifest_events =
   let help = "Outgoing docker push manifest events" in
-  Counter.v ~help ~namespace ~subsystem "docker_push_manifest_events"
+  Counter.v ~help ~namespace ~subsystem "push_manifest_events"
 
 let docker_peek_events =
   let help = "Incoming docker peek events" in
-  Counter.v ~help ~namespace ~subsystem "docker_peek_events"
+  Counter.v ~help ~namespace ~subsystem "peek_events"
 
 let docker_build_events =
   let help = "Docker build events" in
-  Counter.v ~help ~namespace ~subsystem "docker_build_events"
+  Counter.v ~help ~namespace ~subsystem "build_events"
