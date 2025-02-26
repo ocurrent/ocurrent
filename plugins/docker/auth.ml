71edf9ec206977b6198b876c6cbe62b82aaf19df
Mark Elvers <mark.elvers@tunbury.org>
2025-02-26 07:45:02 +0000

    Fix docker auth when using push manifest

--- plugins/docker/auth.ml
+++ plugins/docker/auth.ml
@@ -7,8 +7,8 @@ type auth = {
 let v ~auth ~server =
   Option.map (fun (user, password) -> { user; password; server }) auth
 
-let login ~docker_context ~job = function
+let login ?config ~docker_context ~job = function
   | None -> Lwt.return (Ok ())
   | Some { user; password; server } ->
-      let cmd = Cmd.login ~docker_context ?server user in
+      let cmd = Cmd.login ?config ~docker_context ?server user in
       Current.Process.exec ~cancellable:true ~job ~stdin:password cmd
